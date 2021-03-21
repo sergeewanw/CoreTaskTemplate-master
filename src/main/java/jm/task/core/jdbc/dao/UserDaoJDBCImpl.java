510a13d2d38c0cbdaab04c640ee8b01e55fea2a3
Natalia Sergeeva <tenebresfor@gmail.com>
2021-03-21 21:30:34 +0300

    После правок

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -7,62 +7,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private Connection connection = Util.getConnection();
-    private Statement statement = connection.createStatement();
-    private final String TABLE_NAME = "users";
-
-    public UserDaoJDBCImpl() throws SQLException, ClassNotFoundException {
-    }
 
     public void createUsersTable() {
-        final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (id INT AUTO_INCREMENT, name VARCHAR(256) NOT NULL, lastName VARCHAR(256) NOT NULL, age TINYINT NOT NULL, PRIMARY KEY(id))";
-        try {
-            statement.execute(CREATE_TABLE);
-        } catch (SQLException e) { }
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT, name VARCHAR(256) NOT NULL, lastName VARCHAR(256) NOT NULL, age TINYINT NOT NULL, PRIMARY KEY(id));");
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-        final String DROP_TABLE = "DROP TABLE " + TABLE_NAME;
-        final String SHOW_TABLES = "SHOW TABLES LIKE '" + TABLE_NAME + "'";
-        try {
-            boolean arrayShowTables = statement.execute(SHOW_TABLES);
-            if (arrayShowTables) {
-                statement.execute(DROP_TABLE);
-            }
-        } catch (SQLException e) { }
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("DROP TABLE IF EXISTS users;");
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String INSERT_INTO = "INSERT INTO " + TABLE_NAME + " SET name = '" + name + "', lastName = '" + lastName + "', age = '" + age + "'";
-        try {
-            statement.execute(INSERT_INTO);
-        }catch (SQLException e) { }
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("INSERT INTO users SET name = '" + name + "', lastName = '" + lastName + "', age = '" + age + "';");
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-        final String DELETE_FROM = "DELETE FROM " + TABLE_NAME + " WHERE id = " + id;
-        try {
-            statement.execute(DELETE_FROM);
-        } catch (SQLException e) { }
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("DELETE FROM users WHERE id = " + id + ";");
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        final String SELECT_FROM = "SELECT * FROM " + TABLE_NAME;
         List<User> list = new ArrayList<>();
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_FROM);
+        try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement("SELECT * FROM users;")) {
             ResultSet query = preparedStatement.executeQuery();
             while (query.next()) {
                 list.add(new User(query.getString("name"), query.getString("lastName"), query.getByte("age")));
             }
-        }catch (SQLException e) { }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
         return list;
     }
 
     public void cleanUsersTable() {
-        final String TRUNCATE_TABLE = "TRUNCATE TABLE " + TABLE_NAME;
-        try {
-            statement.execute(TRUNCATE_TABLE);
-        }catch (SQLException e) { }
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("TRUNCATE TABLE users;");
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,21 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "password";
+    private static final String DB_NAME = "schema";
+    private static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/" + DB_NAME;
+    private static Connection connection;
 
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
-        final String DB_USER = "root";
-        final String DB_PASSWORD = "password";
-        return getConnection(DB_USER, DB_PASSWORD);
-    }
-
-    public static Connection getConnection(String dbUser, String dbPassword) throws SQLException, ClassNotFoundException  {
-        final String DB_NAME = "DBTEST";
-        final String CREATE_DATABASE = "CREATE DATABASE " + DB_NAME;
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        System.out.println("Драйвер подключен");
-        String connectionURL = "jdbc:mysql://localhost:3306/" + DB_NAME;
-        Connection connection = DriverManager.getConnection(connectionURL, dbUser, dbPassword);
-        System.out.println("Соединение установлено");
+    public static Connection getConnection() {
+        try  {
+            connection = DriverManager.getConnection(CONNECTION_URL, DB_USER, DB_PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return connection;
     }
 }
